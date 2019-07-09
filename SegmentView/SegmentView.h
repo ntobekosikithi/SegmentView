af74c6436f82a24e9d8dc76379d631d3d1c4cfc7
Ntobeko Sikithi <ntobekosikithi@gmail.com>
2019-07-09 10:24:10 +0200

    Initial commit

---
+++ SegmentView/SegmentView.h
@@ -0,0 +1,19 @@
+//
+//  SegmentView.h
+//  SegmentView
+//
+//  Created by Ntobeko Sikithi on 2019/07/09.
+//  Copyright © 2019 Ntobeko Sikithi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SegmentView.
+FOUNDATION_EXPORT double SegmentViewVersionNumber;
+
+//! Project version string for SegmentView.
+FOUNDATION_EXPORT const unsigned char SegmentViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SegmentView/PublicHeader.h>
+
+
---
+++ SegmentViewExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegmentViewExamples
+//
+//  Created by Ntobeko Sikithi on 2019/07/09.
+//  Copyright © 2019 Ntobeko Sikithi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
